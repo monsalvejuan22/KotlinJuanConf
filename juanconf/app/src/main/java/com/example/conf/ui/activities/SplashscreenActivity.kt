6a5b606e36676c945feda8e3d6b4346200fa36f8
Juan Monsalve <monsalvejuanpablo22@gmail.com>
2021-04-21 20:25:55 -0400

    Comienzo del proyecto

---
+++ juanconf/app/src/main/java/com/example/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
