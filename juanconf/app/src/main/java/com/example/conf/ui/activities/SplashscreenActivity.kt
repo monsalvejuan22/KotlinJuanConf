12b716d7099bd051a4155b95e13845ff664f0224
Juan Monsalve <monsalvejuanpablo22@gmail.com>
2021-05-11 08:40:51 -0400

    animacion lista

--- juanconf/app/src/main/java/com/example/conf/ui/activities/SplashscreenActivity.kt
+++ juanconf/app/src/main/java/com/example/conf/ui/activities/SplashscreenActivity.kt
@@ -1,12 +1,43 @@
 package com.example.conf.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.example.conf.MainActivity
 import com.example.conf.R
+import kotlin.concurrent.timer
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val imagen = findViewById<ImageView>(R.id.imLogoElectromed)
+        val animacion = AnimationUtils.loadAnimation(this,R.anim.animacion)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+
+
+        imagen.startAnimation(animacion)
+
+        animacion.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
     }
 }
